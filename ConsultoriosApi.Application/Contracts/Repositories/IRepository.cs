34ea9610faa62dad0ba0cb0649eb66a31add5a23
vialfmj <vialfmj@gmail.com>
2026-03-29 22:11:27 -0300

    [FIX] - add support for null result in GetById method

--- ConsultoriosApi.Application/Contracts/Repositories/IRepository.cs
+++ ConsultoriosApi.Application/Contracts/Repositories/IRepository.cs
@@ -8,7 +8,7 @@ namespace ConsultoriosApi.Application.Contracts.Repositories
 {
     public interface IRepository<T> where T : class
     {
-        Task<T> GetById(Guid id);
+        Task<T?> GetById(Guid id);
         Task<IEnumerable<T>> GetAll();
         Task<T> Add(T entity);
         Task Update(T entity);
